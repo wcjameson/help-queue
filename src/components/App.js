ea674819d49ba819e4d0f71fcd2d45fb355824dd
wcjameson <williamjameson90@gmail.com>
2022-04-17 16:24:09 -0700

    add initial project setup, components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <Header />
+      <TicketList />
+      {/* This is a JSX comment*/}
+    </React.Fragment>
+  );
+}
+
+export default App;
